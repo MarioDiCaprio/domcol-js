a44a2b2409fbb42215ed00f8decdb9cb5b834481
MarioDiCaprio <mariodicaprio10@gmail.com>
2022-11-09 03:23:16 +0100

    Added MathJax

---
+++ components/MathJax/MathJax.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+
+
+interface MathJaxProps {
+    math: string;
+    raw?: boolean;
+    display?: boolean;
+}
+
+const MathJax: React.FC<MathJaxProps> = ({ math, raw, display = false }) => {
+    const [result, setResult] = useState<JSX.Element>(<></>);
+    let tex = math;
+    if (raw) {
+        tex = String.raw({ raw: math });
+    }
+
+    useEffect(() => {
+        const {MathComponent} = require('mathjax-react');
+        setResult(<MathComponent tex={tex} display={display} />)
+    }, []);
+
+    return result;
+}
+
+
+export default MathJax;
