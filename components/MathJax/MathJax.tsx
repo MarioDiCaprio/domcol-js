e0fb928d526ff4cf85d0c60ad9bb554abc0064a4
MarioDiCaprio <mariodicaprio10@gmail.com>
2022-12-17 23:07:51 +0100

    Improved ssr compatibility

--- components/MathJax/MathJax.tsx
+++ components/MathJax/MathJax.tsx
@@ -8,18 +8,24 @@ interface MathJaxProps {
 }
 
 const MathJax: React.FC<MathJaxProps> = ({ math, raw, display = false }) => {
+    const [isMounted, setIsMounted] = useState<boolean>(false);
     const [result, setResult] = useState<JSX.Element>(<></>);
+
     let tex = math;
     if (raw) {
         tex = String.raw({ raw: math });
     }
 
     useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    useEffect(() => {
         const {MathComponent} = require('mathjax-react');
         setResult(<MathComponent tex={tex} display={display} />)
     }, [math, raw, display]);
 
-    return result;
+    return !isMounted? null : result;
 }
 
 
