4087c83689527dbf1ac32c3c376d3b76a1c9b6c1
MarioDiCaprio <mariodicaprio10@gmail.com>
2023-04-18 14:15:29 +0200

    Updated learn page

--- src/pages/learn.tsx
+++ src/pages/learn.tsx
@@ -2,7 +2,7 @@ import {NextPage} from "next";
 import Base from "../components/Base/Base";
 import MathJax from "../components/MathJax/MathJax";
 import {Container, Paper, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
-import {DocTable, Headline, Link, Paragraph, Section, SubHeadline} from "../styles/Learn.styles";
+import {DocTable, Headline, IllustrationWrapper, Link, Paragraph, Section, SubHeadline} from "../styles/Learn.styles";
 import Credentials from "../components/Credentials/Credentials";
 
 
@@ -18,21 +18,22 @@ const Learn: NextPage = () => {
                     </Headline>
                     <Paragraph>
                         This website was created with the intention of easily plotting complex-valued functions.
-                        More specifically, it utilizes a method called <i>Domain Coloring</i>. This was achieved
-                        with <Link href="src/pages/learn">OpenGL</Link>, implementing the below-described
-                        algorithm using vertex- and fragment shaders.
+                        More specifically, it utilizes the visualization algorithms stated below. This was achieved
+                        with <Link href="https://www.opengl.org/">OpenGL</Link>, which allows to use vertex- and
+                        fragment shaders to achieve our goals.
                     </Paragraph>
                 </Section>
 
                 <Section>
                     <Headline>
-                        What is Domain Coloring?
+                        Domain Coloring
                     </Headline>
                     <Paragraph>
-                        Domain coloring is a method for plotting complex-valued functions. Consider this:
-                        Let <MathJax raw math="f: \mathbb{C} \to \mathbb{C}" /> be a complex-valued function.
+                        <i>Domain Coloring</i> is a method for plotting complex-valued functions. Consider this:
+                        Let <MathJax raw math="f: \mathbb{C} \to \mathbb{C}" /> be a complex-valued function, with <MathJax raw math="z \mapsto f(z)" />.
                         Since <MathJax raw math="\mathbb{C}" /> has two dimensions, we know
-                        that <MathJax math="f" /> accepts two dimensions and outputs two more dimensions.
+                        that both the <Link href="https://en.wikipedia.org/wiki/Domain_of_a_function">domain</Link> (the <i>"input"</i>)
+                        and <Link href="https://en.wikipedia.org/wiki/Codomain">codomain</Link> (the <i>"output"</i>) of <MathJax math="f" /> are two-dimensional.
                         So now we have a total of four dimensions to visualize!
                     </Paragraph>
 
@@ -40,41 +41,69 @@ const Learn: NextPage = () => {
                         The Idea
                     </SubHeadline>
                     <Paragraph>
-                        Let's start with the first two dimensions, namely those given by the function's argument.
-                        The argument can easily be visualized on the complex plane by mapping its real part
-                        to the <MathJax math="x"/>-axis and its imaginary part to the <MathJax math="y"/>-axis.
-                        For the function's output, things become a little bit tricky. Note that a complex number
-                        of the form <MathJax math="a + bi"/> for some <MathJax raw math="a, b \in \mathbb{R}"/> can
-                        be rewritten in its polar form: <MathJax raw math="a + bi = r(\cos \theta + \sin \theta)"/>,
-                        where <MathJax raw math="\theta = \text{atan2}(\frac{b}{a})"/>. If we visualize this number
-                        on the complex plane, as stated before, we will find the following: <MathJax math="r"/> is
-                        the distance from the number to the origin and <MathJax raw math="\theta"/> is the angle from
-                        the number to the <MathJax math="x"/>-axis. This output will not be mapped to a spacial
-                        dimension, but to a color: Let <MathJax raw math="\text{hsv}"/> be some arbitrary function
-                        that accepts hue, saturation and brightness as inputs and outputs a corresponding color.
-                        Then our domain coloring is given by the following:
-                        <MathJax display raw math="\text{Domcol}_f = (a, b, \text{hsv}(\theta, 1, r))"/>
-                        Where each coordinate <MathJax math="(a, b)" /> is colored with its corresponding
-                        color <MathJax raw math="\text{hsv}(\theta, 1, r)"/>.
+                        Let's visualize the function's domain first. Since there exists a one-to-one correspondence
+                        between sets <MathJax raw math="\mathbb{C}" /> and <MathJax raw math="\mathbb{R}^2" />
+                        (<MathJax raw math="a + bi \leftrightarrow (a, b)" />) we can visualize it on the
+                        <i>"classic"</i> two-dimensional coordinate system, here referred to as the {' '}
+                        <Link href="https://en.wikipedia.org/wiki/Complex_plane">complex plane</Link>.
+                        Here, the <MathJax math="x" />-axis will represent all real numbers and the <MathJax math="y" />-axis
+                        will represent all imaginary numbers.
                     </Paragraph>
-
+                    <IllustrationWrapper>
+                        <img alt="complex plane" src="https://upload.wikimedia.org/wikipedia/commons/d/d6/Argandgaussplane.png" />
+                        <div>(source: wikimedia.org)</div>
+                    </IllustrationWrapper>
+                    <Paragraph>
+                        Visualizing the domain was pretty easy. But now comes the tricky part: Visualizing the codomain.
+                        Recall that every complex number can be represented in polar form too: As we see in the illustration
+                        the number <MathJax math="a + bi" /> has an angle <MathJax raw math="\theta" /> (in radians) to the <MathJax math="x" />-axis
+                        and a distance to the origin, which we will call <MathJax math="r" />:
+                        <MathJax raw display math="a + bi = r \cdot e^{i \theta}" />
+                        We also have a quick look at color theory and recall
+                        the <Link href="https://en.wikipedia.org/wiki/HSL_and_HSV">HSV color model</Link>:
+                    </Paragraph>
+                    <IllustrationWrapper>
+                        <img alt="complex plane" src="https://www.dynamicmath.xyz/images/domain-coloring/wheel.svg" style={{ width: 300 }} />
+                        <div>(source: dynamicmath.xyz)</div>
+                    </IllustrationWrapper>
+                    <Paragraph>
+                        As we can see in the chart above, the color's hue will correspond to the angle <MathJax raw math="\theta" />.
+                        Its brightness will be given by the number <MathJax math="r" />: We need some arbitrary bijective mapping from all
+                        possible values of <MathJax math="r" /> (i.e. <MathJax raw math="\mathbb{R}^+" />) to the semi-open
+                        interval <MathJax math="[0; 1)" /> (which will be the brightness). Such a mapping can be {' '}
+                        <MathJax raw math="r \mapsto \frac{\tanh(r)+1}{2}" />, for example. The color's saturation will
+                        be <MathJax math="1" />
+                    </Paragraph>
+                    <Paragraph>
+                        And that's it! To summarize, every number on the complex plane is colored as follows:
+                        <MathJax raw display math="a + bi \mapsto \text{hsv}(h(\theta), 1, b(r))" />
+                        With the following specifications:
+                        <ul>
+                            <li>
+                                <MathJax raw math="h:[0; 2\pi) \to [0; 360)" /> is continuous and bijective
+                                (ideally, simply convert radians to degrees).
+                            </li>
+                            <li>
+                                <MathJax raw math="b:\mathbb{R}^+ \to [0; 1)" /> is continuous and bijective.
+                            </li>
+                        </ul>
+                    </Paragraph>
                     <SubHeadline>
                         Grid Decorations
                     </SubHeadline>
                     <Paragraph>
                         Additionally, we can add decorative grids in order to better visualize certain properties
                         of the function. Currently, there are two types of grids:
+                        <ul>
+                            <li>
+                                dark grid lines are colored whenever the real or imaginary part of the function
+                                are integers
+                            </li>
+                            <li>
+                                light grid lines are colored whenever the function doubles in magnitude
+                            </li>
+                        </ul>
                     </Paragraph>
-
-                    <ul>
-                        <li>
-                            dark grid lines are colored whenever the real or imaginary part of the function
-                            are integers
-                        </li>
-                        <li>
-                            light grid lines are colored whenever the function doubles in magnitude
-                        </li>
-                    </ul>
                 </Section>
 
                 <Section>
--- src/styles/Learn.styles.tsx
+++ src/styles/Learn.styles.tsx
@@ -32,3 +32,16 @@ export const DocTable = styled(Table)`
 export const Link = styled('a')`
   color: ${props => props.theme.palette.primary.main};
 `;
+
+export const IllustrationWrapper = styled('div')`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  margin: 10px 0 10px 0;
+  color: ${props => props.theme.palette.text.disabled};
+  img {
+    border-radius: 5px;
+  }
+`;
